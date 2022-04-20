c5cc7fea9db1b70084bfbb869531ea35fc8bcbf2
João Vitor Madureira <103207658+j-madureira@users.noreply.github.com>
2022-04-20 13:45:49 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+/*Crie um array e utilize for para fazer uma varredura
+*/
+
+var pessoas = ["Jonathan","João","Rafaele","Tamires","Dierbo"];
+
+for(var i = 0; i < pessoas.length; ++i){
+    console.log(pessoas[i]);
+}
